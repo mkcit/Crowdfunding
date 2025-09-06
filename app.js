beedfe2f8bc02d84f0d9a305fbed35dd92d1768a
mks.cit <mks.cit@outlook.com>
2025-09-06 20:22:03 +0300

    install express

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 // http is object containing methods and properties and classes
 const http = require('http');
 
-// createServer is a method of http object
-// server is an instance of http.Server class (object)
-const server = http.createServer((req, res) => {
-    
-});
+// express is a function returning an object
+const express = require('express');
+
+// app is an object containing methods and properties
+const app = express();
+
 
 const PORT = 3000;
-server.listen(PORT);
-console.log('Listening on port 3000');
+app.listen(PORT, () => {
+    console.log('Listening on port 4000');
+});
+
