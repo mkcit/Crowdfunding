1cb1537032433fe17a4491d7fc4bc24995d5dd6d
mks.cit <mks.cit@outlook.com>
2025-09-06 20:32:59 +0300

    Test middlewares and google redirecting

--- app.js
+++ app.js
@@ -2,15 +2,27 @@
 // http is object containing methods and properties and classes
 const http = require('http');
 
-// express is a function returning an object
+// express is a function
 const express = require('express');
 
+// express is a function that returns an object
 // app is an object containing methods and properties
 const app = express();
 
+// middleware function
+// req is request object
+// res is response object
+// next is a function that calls the next middleware function in the stack
+app.use((req, res, next) => {
+    console.log(req.url + ' ' + req.method);
+    next();
+});
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log('Listening on port 4000');
+app.use((req, res, next) => {
+    res.redirect('https://www.google.ps');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
 });
 
